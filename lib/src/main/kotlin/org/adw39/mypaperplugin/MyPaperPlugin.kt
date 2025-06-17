dd94c0225b1753c178ecd5ded34ee74288720bd9
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-18 03:04:48 +0900

    ファイル作成
    公式リファレンス通り

--- lib/src/main/kotlin/org/adw39/mypaperplugin/MyPaperPlugin.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/MyPaperPlugin.kt
@@ -1,4 +1,19 @@
 package org.adw39.mypaperplugin
 
-class MyPaperPlugin {
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+class MyPaperPlugin: JavaPlugin(), Listener {
+    override fun onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this)
+    }
+
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        event.player.sendMessage(Component.text("Welcome back!", NamedTextColor.GREEN))
+    }
 }
