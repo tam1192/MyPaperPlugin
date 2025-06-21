3c68f193d96217bd83e9e9a9007b1847aada8b0f
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-21 14:55:50 +0900

    一括破壊もどき作ってみた

---
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/IkkatuHakai.kt
@@ -0,0 +1,19 @@
+package org.adw39.mypaperplugin
+
+import org.bukkit.Material
+import org.bukkit.block.Block
+
+object IkkatuHakai {
+    fun ikkatuHakai(block: Block) {
+        if (block.type === Material.OAK_LOG) {
+            block.breakNaturally()
+            for (i in 0..<26) {
+                val x = (i % 3)-1
+                val y = (i / 3 % 3)-1
+                val z = (i / 9)-1
+                val location = block.location.add(x.toDouble(), y.toDouble(), z.toDouble())
+                ikkatuHakai(location.block)
+            }
+        }
+    }
+}
--- lib/src/main/kotlin/org/adw39/mypaperplugin/PlayerListener.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/PlayerListener.kt
@@ -1,7 +1,10 @@
 package org.adw39.mypaperplugin
 
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -10,5 +13,10 @@ class PlayerListener: Listener{
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         event.player.sendMessage("Welcome to the server!")
+    }
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) {
+        IkkatuHakai.ikkatuHakai(event.block)
     }
 }
