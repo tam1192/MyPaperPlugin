f05d0b6d9f20998675308e805aa85d2f0bd39857
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-22 16:54:24 +0900

    一括破壊できるようにした。

--- lib/src/main/kotlin/org/adw39/mypaperplugin/IkkatuHakai.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/IkkatuHakai.kt
@@ -5,15 +5,18 @@ import org.bukkit.block.Block
 
 object IkkatuHakai {
     fun ikkatuHakai(block: Block) {
-        val targetBlock = MyPaperPlugin.instance.config.getList("targetBlock")?.filterIsInstance<Material>() ?: mutableListOf<Material>()
-        if (block.type in targetBlock) {
+        val targetBlock = MyPaperPlugin.instance.config.getStringList("targetBlock")
+        // targetBlockの中に存在すれば
+        if (block.type.name.lowercase() in targetBlock) {
             block.breakNaturally()
             for (i in 0..<26) {
                 val x = (i % 3)-1
                 val y = (i / 3 % 3)-1
                 val z = (i / 9)-1
                 val location = block.location.add(x.toDouble(), y.toDouble(), z.toDouble())
-                ikkatuHakai(location.block)
+                if (location.block.type == block.type) {
+                    ikkatuHakai(location.block)
+                }
             }
         }
     }
--- lib/src/main/kotlin/org/adw39/mypaperplugin/commands/IkkatuHakai.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/commands/IkkatuHakai.kt
@@ -6,6 +6,8 @@ import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
 import org.bukkit.Material
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
 import java.util.logging.Logger
 
 class IkkatuHakai: TabExecutor {
@@ -26,8 +28,13 @@ class IkkatuHakai: TabExecutor {
             return result
         }
         return when(arg1) {
-            "add", "del" -> {
+            // 極力lowercaseで登録させる
+            "add" -> {
                 Material.entries.filter { it.isBlock }.map { it.name.lowercase() }.toMutableList()
+            }
+            // リストの中から選ばせる
+            "del" -> {
+                MyPaperPlugin.instance.config.getStringList("targetBlock")
             }
             else -> null
         }
@@ -37,52 +44,70 @@ class IkkatuHakai: TabExecutor {
         val args = p3.iterator()
         val arg1 = args.next()
         if (arg1.isBlank()) {return false}
-        val targetBlock = MyPaperPlugin.instance.config.getList("targetBlock")?.filterIsInstance<Material>() ?: mutableListOf<Material>()
+        val targetBlock = MyPaperPlugin.instance.config.getStringList("targetBlock")
         return when(arg1) {
-            "add" -> {
-                val val1 = kotlin.runCatching { args.next() }.getOrElse { return false }.uppercase()
-                val material = Material.getMaterial(val1) ?: run {
-                    sender.sendMessage("ブロックidが存在しません")
-                    return true
+            "add", "del" -> {
+                // 存在しなければ、プレイヤー目の前のブロックを取得してみる
+                val val1 = kotlin.runCatching { args.next() }.getOrElse {
+                    if (sender is Player) {
+                        val val1 = sender.getTargetBlock(null, 10).type
+                        if (val1 != Material.AIR) {
+                            val1.name.lowercase()
+                        } else {
+                            return false
+                        }
+                    } else {
+                        return false
+                    }
+                }.lowercase()
+                // マテリアルかつblockIdであることを確認する
+                Material.getMaterial(val1.uppercase()).also {
+                    if ( it === null ) {
+                        sender.sendMessage("このブロックidは存在しません")
+                        return true
+                    } else if (!it.isBlock) {
+                        sender.sendMessage("これはブロックidではありません")
+                        return true
+                    }
                 }
-                if (!material.isBlock) {
-                    sender.sendMessage("ブロックidではありません。")
-                    return true
-                }
-                if (targetBlock.find { it == material } == null) {
-                    MyPaperPlugin.instance.config.set("targetBlock", (targetBlock + material))
-                    MyPaperPlugin.instance.saveConfig()
-                    true
+                // すでにリストに登録されているか
+                val isExistTargetBlock = val1 in targetBlock
+                // add or delにより挙動が変わる
+                if (arg1=="add") {
+                    // リストに存在しなければ追加
+                    if (!isExistTargetBlock) {
+                        MyPaperPlugin.instance.config.set("targetBlock", (targetBlock + val1))
+                        sender.sendMessage("${val1}を登録しました")
+                        true
+                    } else {
+                        sender.sendMessage("このブロックidはすでに登録されています")
+                        true
+                    }
                 } else {
-                    sender.sendMessage("このブロックidはすでに登録されています")
-                    true
+                    // リストに存在すれば削除
+                    if (isExistTargetBlock) {
+                        targetBlock.filter { it != val1 }
+                            .also { MyPaperPlugin.instance.config.set("targetBlock", it) }
+                        sender.sendMessage("${val1}を削除しました。")
+                        true
+                    } else {
+                        sender.sendMessage("このブロックidはすでに削除されています")
+                        true
+                    }
                 }
-            }
-            "del" -> {
-                val val1 = kotlin.runCatching { args.next() }.getOrElse { return false }
-                val material = Material.getMaterial(val1) ?: run {
-                    sender.sendMessage("ブロックidが存在しません")
-                    return true
-                }
-                targetBlock.filter { it != material }.also {
-                    MyPaperPlugin.instance.config.set("targetBlock", it)
-                    sender.sendMessage("${val1.lowercase()}を削除しました。")
-                }
-                true
+
             }
             "list" -> {
                 if (targetBlock.isEmpty()) {
                     sender.sendMessage("ブロックが登録されていません。")
                     true
                 } else {
-                    var sendMessage = "登録済みのブロック\n"
-                    targetBlock.forEach {
-                        sendMessage += "- ${it.name}\n"
-                    }
+                    // - で区切って表示
+                    var sendMessage = "登録済みのブロック\n\t- "
+                    sendMessage += targetBlock.joinToString("\n\t- ")
                     sender.sendMessage(sendMessage)
                     true
                 }
-
             }
             else -> false
         }
