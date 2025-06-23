97b10669f68c0f9d7ffa632808ffc919ecea52f3
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-24 02:56:18 +0900

    blockAroundSearchを作った

--- lib/src/main/kotlin/org/adw39/mypaperplugin/commands/ExVector.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/commands/ExVector.kt
@@ -1,6 +1,7 @@
 package org.adw39.mypaperplugin.commands
 
 import org.adw39.mypaperplugin.MyPaperPlugin
+import org.adw39.mypaperplugin.utils.blockAroundSearch
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -21,6 +22,11 @@ class ExVector: TabExecutor {
                 sender.sendMessage("normal block")
                 true
             }
+            args.size == 1 && fixArgs[0] == "test1" && sender is Player -> {
+                val target = sender.getTargetBlock(null, 10)
+                blockAroundSearch(target)
+                true
+            }
             args.size == 2 && fixArgs[0] == "distance_target_block" && fixArgs[1] == "normal" && sender is Player  -> {
                 val player = sender.location.toVector()
                 val targetBlock = sender.getTargetBlock(null, 100).location.toVector()
@@ -34,7 +40,7 @@ class ExVector: TabExecutor {
                 true
             }
             else -> {
-                sender.sendMessage("location_vs_vector distance_target_block")
+                sender.sendMessage("location_vs_vector distance_target_block test1")
                 false
             }
         }
@@ -44,7 +50,7 @@ class ExVector: TabExecutor {
         return when {
             args.size == 2 && args[0] == "location_vs_vector" -> ""
             args.size == 2 && args[0] == "distance_target_block" -> "normal,block"
-            args.size == 1 && args[0] == "" -> "location_vs_vector,distance_target_block"
+            args.size == 1 && args[0] == "" -> "location_vs_vector,distance_target_block,test1"
             else -> ""
         }.split(",").toMutableList()
     }
---
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/utils/BlockSearcher.kt
@@ -0,0 +1,32 @@
+package org.adw39.mypaperplugin.utils
+
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.block.data.BlockData
+import org.bukkit.entity.Player
+import org.bukkit.util.Vector
+
+//fun blockChainSearch(centerBlock: Block) {
+//    val world = centerBlock.world
+//    val
+//
+//
+//}
+
+fun blockForwardSearch(centerBlock: Block, targetVec: Vector) {
+
+}
+
+fun blockAroundSearch(centerBlock: Block) {
+    val world = centerBlock.world
+    val centerVec = centerBlock.location.toVector()
+    for (x in -1..1) {
+        for (y in -1..1) {
+            for (z in -1..1) {
+                if (x != 0 || y != 0 || z != 0) {
+                    Vector(x, y, z).add(centerVec).toLocation(world).block.type = Material.GLASS
+                }
+            }
+        }
+    }
+}
