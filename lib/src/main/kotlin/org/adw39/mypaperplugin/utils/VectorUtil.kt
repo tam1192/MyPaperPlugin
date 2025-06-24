726da2ce8c35d25ca293b6c7b6b324312da250fb
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-24 15:50:58 +0900

    重複防止？

--- lib/src/main/kotlin/org/adw39/mypaperplugin/utils/VectorUtil.kt
+++ lib/src/main/kotlin/org/adw39/mypaperplugin/utils/VectorUtil.kt
@@ -13,32 +13,49 @@ import kotlin.math.sign
 //
 //}
 
+// previous(手前)とcurrent(対象ブロック、現在)から、自分と同じブロックを取得する
+fun blockForwardSearch(previousVec: BlockVector, current: Block): List<BlockVector> {
+    val targetBlockType = current.type
+    val world = current.world
+    val currentVec = BlockVector(current.x, current.y, current.z)
+    return blockForwardSearchTarget(previousVec, currentVec).filter {
+        val locBlock = it.toLocation(world).block
+        locBlock.type === targetBlockType
+    }
+}
+
 // previous(手前)とcurrent(対象ブロック、現在)から、探索が必要なブロックを取得する
-fun blockForwardSearch(previousVec: BlockVector, currentVec: BlockVector): List<BlockVector> {
+fun blockForwardSearchTarget(previousVec: BlockVector, currentVec: BlockVector): List<BlockVector> {
     // どの方向に進んでるかを取得する
     val direction = run {
         // 現在と手前の差分で、進んでる方向を26方向で表す
         val dif = currentVec.clone().subtract(previousVec)
-        Triple(sign(dif.x).toInt(), sign(dif.y).toInt(), sign(dif.z).toInt())
+        Triple(sign(dif.x).toInt()+1, sign(dif.y).toInt()+1, sign(dif.z).toInt()+1)
     }
 
-    // currentの1つ外側に対してブロックを設置する。
-    val res = mutableListOf<BlockVector>()
+    // 探索が必要な部分をtrueにする
+    val res = MutableList(3) {
+        MutableList(3) {
+            MutableList(3) {
+                false
+            }
+        }
+    }
 
     // 次に探索&進むべき方向を表す
-    for (a in -1..1) {
-        for (b in -1..1) {
+    for (a in 0..2) {
+        for (b in 0..2) {
             // x軸方向に進んでる
             if (direction.first != 0) {
-                res.add(BlockVector(direction.first + currentVec.blockX, a + currentVec.blockY, b + currentVec.blockZ))
+                res[direction.first][a][b] = true
             }
             // y軸方向に進んでる
             if (direction.second != 0) {
-                res.add(BlockVector(a + currentVec.blockX, direction.second + currentVec.blockY, b + currentVec.blockZ))
+                res[a][direction.second][b] = true
             }
             // z軸方向に進んでる
             if (direction.third != 0) {
-                res.add(BlockVector(a + currentVec.blockX, b + currentVec.blockY, direction.third + currentVec.blockZ))
+                res[a][b][direction.third] = true
             }
         }
     }
